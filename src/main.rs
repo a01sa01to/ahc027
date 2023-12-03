43e65f96226c79a16087add5f6bebb8b3ce8c90f
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-12-03 07:32:07 +0000

    bug fix 2

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ const RIGHT: Direction = 1;
 const DOWN: Direction = 2;
 const LEFT: Direction = 3;
 
-const DX: [i64; 4] = [0, 1, 0, -1];
-const DY: [i64; 4] = [-1, 0, 1, 0];
+const DI: [i64; 4] = [-1, 0, 1, 0];
+const DJ: [i64; 4] = [0, 1, 0, -1];
 
 fn can_move(
     (i, j): (usize, usize),
@@ -29,14 +29,14 @@ fn can_move(
             }
         }
         RIGHT => {
-            if j == wall_vertical[i].len() {
+            if j + 1 == wall_horizontal[0].len() {
                 false
             } else {
                 wall_vertical[i][j] == '0'
             }
         }
         DOWN => {
-            if i == wall_horizontal.len() {
+            if i + 1 == wall_vertical.len() {
                 false
             } else {
                 wall_horizontal[i][j] == '0'
@@ -56,7 +56,7 @@ fn can_move(
 fn dfs(
     (i, j): (usize, usize),
     ans: &mut Vec<(usize, usize)>,
-    wants_turn: &mut Vec<Vec<(VecDeque<usize>, usize)>>,
+    wants_turn: &mut Vec<Vec<(VecDeque<i32>, i32)>>,
     wall_horizontal: &Vec<Vec<char>>,
     wall_vertical: &Vec<Vec<char>>,
     turn: usize,
@@ -67,10 +67,10 @@ fn dfs(
     }
 
     // 次に行く場所を探す
-    let mut nxt = BinaryHeap::<Reverse<(usize, (usize, usize))>>::new();
+    let mut nxt = BinaryHeap::<Reverse<(i32, (usize, usize, i32))>>::new();
     for dir in 0..4 {
         if can_move((i, j), dir, wall_horizontal, wall_vertical) {
-            let (ni, nj) = ((i as i64 + DX[dir]) as usize, (j as i64 + DY[dir]) as usize);
+            let (ni, nj) = ((i as i64 + DI[dir]) as usize, (j as i64 + DJ[dir]) as usize);
             let offset = wants_turn[ni][nj].1;
             let wants = &mut wants_turn[ni][nj].0;
             if wants.is_empty() {
@@ -78,9 +78,9 @@ fn dfs(
             }
             let want = wants.pop_front().unwrap();
             if offset == 0 && (ni, nj) != (0, 0) {
-                nxt.push(Reverse((0, (ni, nj))));
+                nxt.push(Reverse((0, (ni, nj, offset))));
             } else {
-                nxt.push(Reverse((want + offset - turn, (ni, nj))));
+                nxt.push(Reverse((want + offset - turn as i32, (ni, nj, offset))));
             }
         }
     }
@@ -88,7 +88,10 @@ fn dfs(
         return false;
     }
 
-    while let Some(Reverse((want1, (ni, nj)))) = nxt.pop() {
+    while let Some(Reverse((want1, (ni, nj, offset)))) = nxt.pop() {
+        if offset == 0 {
+            wants_turn[ni][nj].1 = turn as i32;
+        }
         if dfs(
             (ni, nj),
             ans,
@@ -100,8 +103,10 @@ fn dfs(
             ans[turn] = (ni, nj);
             return true;
         }
-        let offset = wants_turn[ni][nj].1;
-        wants_turn[ni][nj].0.push_front(want1 - offset + turn);
+        wants_turn[ni][nj]
+            .0
+            .push_front(want1 - offset + turn as i32);
+        wants_turn[ni][nj].1 = offset;
     }
 
     false
@@ -112,11 +117,11 @@ fn main() {
         n: usize,
         wall_horizontal: [Chars; n - 1],
         wall_vertical: [Chars; n],
-        dirtiness: [[usize; n]; n]
+        dirtiness: [[i32; n]; n]
     };
 
     // 何回訪れたいか決める
-    let max_len = 100_000;
+    let max_len = 100_000usize;
     let mut num_visit = dirtiness.clone();
     // 周りを見て不可能なものは減らす
     {
@@ -142,14 +147,14 @@ fn main() {
         let mut len = 0;
         for i in 0..n {
             for j in 0..n {
-                len += num_visit[i][j];
+                len += num_visit[i][j] as usize;
             }
         }
 
         if len > max_len {
             for i in 0..n {
                 for j in 0..n {
-                    num_visit[i][j] = num_visit[i][j] * max_len / len;
+                    num_visit[i][j] = num_visit[i][j] * max_len as i32 / len as i32;
                 }
             }
         }
@@ -160,7 +165,7 @@ fn main() {
     let mut len = 0;
     for i in 0..n {
         for j in 0..n {
-            len += num_visit[i][j];
+            len += num_visit[i][j] as usize;
         }
     }
     assert!(
@@ -171,11 +176,13 @@ fn main() {
     );
 
     // いつ行きたいか求める
-    let mut wants_turn = vec![vec![(VecDeque::<usize>::new(), 0); n]; n];
+    let mut wants_turn = vec![vec![(VecDeque::<i32>::new(), 0); n]; n];
     for i in 0..n {
         for j in 0..n {
             for k in 0..num_visit[i][j] {
-                wants_turn[i][j].0.push_back(k * len / num_visit[i][j]);
+                wants_turn[i][j]
+                    .0
+                    .push_back(k * len as i32 / num_visit[i][j]);
             }
         }
     }
