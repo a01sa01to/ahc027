304919c9a20dfa73ebacea0dbd9a717c113983d6
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-12-03 06:39:32 +0000

    Direction の書き方を変更

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
-enum Direction {
-    Up = 0,
-    Right = 1,
-    Down = 2,
-    Left = 3,
-}
+type Direction = usize;
+const UP: Direction = 0;
+const RIGHT: Direction = 1;
+const DOWN: Direction = 2;
+const LEFT: Direction = 3;
+
+const DX: [i64; 4] = [0, 1, 0, -1];
+const DY: [i64; 4] = [-1, 0, 1, 0];
 
 fn can_move(
     (i, j): (usize, usize),
@@ -14,28 +18,28 @@ fn can_move(
     wall_vertical: &Vec<Vec<char>>,
 ) -> bool {
     match direction {
-        Direction::Up => {
+        UP => {
             if i == 0 {
                 false
             } else {
                 wall_horizontal[i - 1][j] == '.'
             }
         }
-        Direction::Right => {
+        RIGHT => {
             if j == wall_vertical[i].len() {
                 false
             } else {
                 wall_vertical[i][j] == '.'
             }
         }
-        Direction::Down => {
+        DOWN => {
             if i == wall_horizontal.len() {
                 false
             } else {
                 wall_horizontal[i][j] == '.'
             }
         }
-        Direction::Left => {
+        LEFT => {
             if j == 0 {
                 false
             } else {
@@ -43,6 +47,34 @@ fn can_move(
             }
         }
     }
+}
+
+fn dfs(
+    (i, j): (usize, usize),
+    ans: &mut Vec<(usize, usize)>,
+    wants_turn: &mut Vec<Vec<(VecDeque<usize>, usize)>>,
+    wall_horizontal: &Vec<Vec<char>>,
+    wall_vertical: &Vec<Vec<char>>,
+    turn: usize,
+) -> bool {
+    // 最後のターンは確定
+    if turn == ans.len() + 1 {
+        return (i, j) == (0, 0);
+    }
+
+    // 次に行く場所を決める
+    let mut nxt = Vec::<(usize, usize)>::new();
+    for dir in 0..4 {
+        if can_move((i, j), dir, wall_horizontal, wall_vertical) {
+            let (ni, nj) = (
+                (i as i64 + DX[dir as usize]) as usize,
+                (j as i64 + DY[dir as usize]) as usize,
+            );
+            nxt.push((ni, nj));
+        }
+    }
+
+    false
 }
 
 fn main() {
@@ -61,16 +93,16 @@ fn main() {
         for i in 0..n {
             for j in 0..n {
                 let mut dig = 0;
-                if can_move((i, j), Direction::Up, &wall_horizontal, &wall_vertical) {
+                if can_move((i, j), UP, &wall_horizontal, &wall_vertical) {
                     dig += num_visit[i - 1][j];
                 }
-                if can_move((i, j), Direction::Right, &wall_horizontal, &wall_vertical) {
+                if can_move((i, j), RIGHT, &wall_horizontal, &wall_vertical) {
                     dig += num_visit[i][j + 1];
                 }
-                if can_move((i, j), Direction::Down, &wall_horizontal, &wall_vertical) {
+                if can_move((i, j), DOWN, &wall_horizontal, &wall_vertical) {
                     dig += num_visit[i + 1][j];
                 }
-                if can_move((i, j), Direction::Left, &wall_horizontal, &wall_vertical) {
+                if can_move((i, j), LEFT, &wall_horizontal, &wall_vertical) {
                     dig += num_visit[i][j - 1];
                 }
                 num_visit[i][j] = num_visit[i][j].min(dig / 2);
@@ -109,20 +141,29 @@ fn main() {
     );
 
     // いつ行きたいか求める
-    let mut wants_turn = vec![vec![Vec::<usize>::new(); n]; n];
+    let mut wants_turn = vec![vec![(VecDeque::<usize>::new(), 0); n]; n];
     for i in 0..n {
         for j in 0..n {
             for k in 0..num_visit[i][j] {
-                wants_turn[i][j].push(k * len / num_visit[i][j]);
+                wants_turn[i][j].0.push_back(k * len / num_visit[i][j]);
             }
         }
     }
 
     // いい感じに求める
     let mut ans = vec![(!0, !0); len];
+    ans[0] = (0, 0);
+    dfs(
+        (0, 0),
+        &mut ans,
+        &mut wants_turn,
+        &wall_horizontal,
+        &wall_vertical,
+        0,
+    );
 
     // output
-    for i in 0..len {
+    for i in 0..len + 1 {
         let (nowi, nowj) = ans[i];
         let (nxti, nxtj) = ans[(i + 1) % len];
         assert!(
@@ -137,22 +178,22 @@ fn main() {
         );
         let dir = {
             if nowi + 1 == nxti {
-                Direction::Down
+                DOWN
             } else if nowi == nxti + 1 {
-                Direction::Up
+                UP
             } else if nowj + 1 == nxtj {
-                Direction::Right
+                RIGHT
             } else if nowj == nxtj + 1 {
-                Direction::Left
+                LEFT
             } else {
                 unreachable!()
             }
         };
         match dir {
-            Direction::Up => print!("U"),
-            Direction::Right => print!("R"),
-            Direction::Down => print!("D"),
-            Direction::Left => print!("L"),
+            UP => print!("U"),
+            RIGHT => print!("R"),
+            DOWN => print!("D"),
+            LEFT => print!("L"),
         }
     }
     println!();
