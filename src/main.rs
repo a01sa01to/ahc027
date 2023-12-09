e86461270a02012e94cf443fb44545a69c9c5d82
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-12-09 09:59:14 +0000

    何もかも変えた first

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, VecDeque},
-};
+use std::thread;
 
 use proconio::{input, marker::Chars};
 
@@ -13,6 +10,8 @@ const LEFT: Direction = 3;
 
 const DI: [i64; 4] = [-1, 0, 1, 0];
 const DJ: [i64; 4] = [0, 1, 0, -1];
+
+const MAX_LEN: usize = 100_000;
 
 fn can_move(
     (i, j): (usize, usize),
@@ -54,155 +53,104 @@ fn can_move(
 }
 
 fn dfs(
-    (i, j): (usize, usize),
     ans: &mut Vec<(usize, usize)>,
-    wants_turn: &mut Vec<Vec<(VecDeque<i32>, i32)>>,
+    visited: &mut Vec<Vec<u32>>,
+    cand: &mut Vec<(u32, (usize, usize))>,
+    now: &mut (usize, usize),
     wall_horizontal: &Vec<Vec<char>>,
     wall_vertical: &Vec<Vec<char>>,
-    turn: usize,
-) -> bool {
-    // 最後のターンは確定
-    if turn == ans.len() + 1 {
-        return (i, j) == (0, 0);
+    dirtiness: &Vec<Vec<u32>>,
+    dep: &mut u32,
+) {
+    if *dep > MAX_LEN as u32 {
+        return;
     }
 
-    // 次に行く場所を探す
-    let mut nxt = BinaryHeap::<Reverse<(i32, (usize, usize, i32))>>::new();
-    for dir in 0..4 {
-        if can_move((i, j), dir, wall_horizontal, wall_vertical) {
-            let (ni, nj) = ((i as i64 + DI[dir]) as usize, (j as i64 + DJ[dir]) as usize);
-            let offset = wants_turn[ni][nj].1;
-            let wants = &mut wants_turn[ni][nj].0;
-            if wants.is_empty() {
-                continue;
-            }
-            let want = wants.pop_front().unwrap();
-            if offset == 0 && (ni, nj) != (0, 0) {
-                nxt.push(Reverse((0, (ni, nj, offset))));
+    let (i, j) = *now;
+    visited[i][j] = *dep;
+    for direction in 0..4 {
+        if can_move((i, j), direction, wall_horizontal, wall_vertical) {
+            let (ni, nj) = (
+                (i as i64 + DI[direction]) as usize,
+                (j as i64 + DJ[direction]) as usize,
+            );
+            let di = if visited[ni][nj] == 0 && *now != (0, 0) {
+                1_000_000
             } else {
-                nxt.push(Reverse((want + offset - turn as i32, (ni, nj, offset))));
-            }
+                (*dep - visited[ni][nj]) * dirtiness[ni][nj]
+            };
+            cand.push((di, (ni, nj)));
         }
-    }
-    if nxt.is_empty() {
-        return false;
     }
 
-    while let Some(Reverse((want1, (ni, nj, offset)))) = nxt.pop() {
-        if offset == 0 {
-            wants_turn[ni][nj].1 = turn as i32;
-        }
-        if dfs(
-            (ni, nj),
-            ans,
-            wants_turn,
-            wall_horizontal,
-            wall_vertical,
-            turn + 1,
-        ) {
-            ans[turn] = (ni, nj);
-            return true;
-        }
-        wants_turn[ni][nj]
-            .0
-            .push_front(want1 - offset + turn as i32);
-        wants_turn[ni][nj].1 = offset;
+    if cand.len() == 0 {
+        return;
     }
 
-    false
+    cand.sort();
+    let (_, (ni, nj)) = cand.last().unwrap();
+    ans.push((*ni, *nj));
+    *now = (*ni, *nj);
+    *dep += 1;
+    cand.clear();
+
+    dfs(
+        ans,
+        visited,
+        cand,
+        now,
+        wall_horizontal,
+        wall_vertical,
+        dirtiness,
+        dep,
+    );
 }
 
-fn main() {
+fn real_main() {
     input! {
         n: usize,
         wall_horizontal: [Chars; n - 1],
         wall_vertical: [Chars; n],
-        dirtiness: [[i32; n]; n]
+        dirtiness: [[u32; n]; n]
     };
 
-    // 何回訪れたいか決める
-    let max_len = 100_000usize;
-    let mut num_visit = dirtiness.clone();
-    // 周りを見て不可能なものは減らす
-    {
-        for i in 0..n {
-            for j in 0..n {
-                let mut deg = 0;
-                if can_move((i, j), UP, &wall_horizontal, &wall_vertical) {
-                    deg += dirtiness[i - 1][j];
-                }
-                if can_move((i, j), RIGHT, &wall_horizontal, &wall_vertical) {
-                    deg += dirtiness[i][j + 1];
-                }
-                if can_move((i, j), DOWN, &wall_horizontal, &wall_vertical) {
-                    deg += dirtiness[i + 1][j];
-                }
-                if can_move((i, j), LEFT, &wall_horizontal, &wall_vertical) {
-                    deg += dirtiness[i][j - 1];
-                }
-                num_visit[i][j] = dirtiness[i][j].min(deg / 2);
-            }
-        }
-
-        let mut len = 0;
-        for i in 0..n {
-            for j in 0..n {
-                len += num_visit[i][j] as usize;
-            }
-        }
-
-        if len > max_len {
-            for i in 0..n {
-                for j in 0..n {
-                    num_visit[i][j] = num_visit[i][j] * max_len as i32 / len as i32;
-                }
-            }
-        }
-    }
-
-    // 行動の長さを決める
-    let num_visit = num_visit;
-    let mut len = 0;
-    for i in 0..n {
-        for j in 0..n {
-            len += num_visit[i][j] as usize;
-        }
-    }
-    assert!(
-        len <= max_len,
-        "len is {}, which is larger than max_len {}",
-        len,
-        max_len
-    );
-
-    // いつ行きたいか求める
-    let mut wants_turn = vec![vec![(VecDeque::<i32>::new(), 0); n]; n];
-    for i in 0..n {
-        for j in 0..n {
-            for k in 0..num_visit[i][j] {
-                wants_turn[i][j]
-                    .0
-                    .push_back(k * len as i32 / num_visit[i][j]);
-            }
-        }
-    }
-
-    // いい感じに求める
-    let mut ans = vec![(!0, !0); len];
-    ans[0] = (0, 0);
+    // solve
+    let mut ans = Vec::<(usize, usize)>::new();
+    ans.push((0, 0));
+    let mut visited = vec![vec![0; n]; n];
+    let mut now = (0, 0);
+    let mut cand = Vec::<(u32, (usize, usize))>::new();
+    let mut dep = 0;
     dfs(
-        (0, 0),
         &mut ans,
-        &mut wants_turn,
+        &mut visited,
+        &mut cand,
+        &mut now,
         &wall_horizontal,
         &wall_vertical,
-        0,
+        &dirtiness,
+        &mut dep,
     );
 
+    // つじつまあわせ
+    while let Some(&(i, j)) = ans.last() {
+        if i == 0 && j == 0 {
+            break;
+        }
+        ans.pop();
+    }
+
+    assert!(
+        ans.last().unwrap() == &(0, 0),
+        "ans.last() = {:?}",
+        ans.last()
+    );
+    assert!(ans.len() - 1 <= MAX_LEN, "ans.len() = {}", ans.len());
+
     // output
-    for i in 0..len + 1 {
+    for i in 0..ans.len() - 1 {
         let (nowi, nowj) = ans[i];
-        let (nxti, nxtj) = ans[(i + 1) % len];
+        let (nxti, nxtj) = ans[(i + 1) % ans.len()];
         assert!(
             (nowi != nxti && nowj == nxtj && (nowi + 1 == nxti || nowi == nxti + 1))
                 || (nowi == nxti && nowj != nxtj && (nowj + 1 == nxtj || nowj == nxtj + 1)),
@@ -235,4 +183,16 @@ fn main() {
         }
     }
     println!();
+}
+
+fn main() {
+    // ローカル Stack Overflow 対策
+    let handler = thread::Builder::new()
+        .stack_size(1024 * 1024 * 1024)
+        .spawn(|| {
+            real_main();
+        })
+        .expect("can't spawn thread");
+
+    handler.join().expect("something's wrong with the thread");
 }
